aa2fe7f5e600d53526e994547a6e40f4be5c29f0
Md Noyon Hossen <noyonhossen1996@gmail.com>
2021-03-02 19:05:27 +0600

    Employee entity added

---
+++ DreamJobs/DreamJobs.Web/Migrations/Framework/20210302130351_EmployeeInitilization.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DreamJobs.Web.Migrations.Framework
+{
+    public partial class EmployeeInitilization : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    UserId = table.Column<Guid>(nullable: false),
+                    FullName = table.Column<string>(nullable: true),
+                    FatherName = table.Column<string>(nullable: true),
+                    MotherName = table.Column<string>(nullable: true),
+                    DateOfBirth = table.Column<DateTime>(nullable: false),
+                    Gender = table.Column<string>(nullable: true),
+                    MobileNo = table.Column<string>(nullable: true),
+                    PresentAddress = table.Column<string>(nullable: true),
+                    PermanentAddress = table.Column<string>(nullable: true),
+                    Skills = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ DreamJobs/Libraries/DreamJobs.Framework/Entities/Employee.cs
@@ -0,0 +1,22 @@
+﻿using DreamJobs.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DreamJobs.Framework.Entities
+{
+    public class Employee : IEntity<Guid>
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string FullName { get; set; }
+        public string FatherName { get; set; }
+        public string MotherName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public string MobileNo { get; set; }
+        public string PresentAddress { get; set; }
+        public string PermanentAddress { get; set; }
+        public string Skills { get; set; }
+    }
+}
