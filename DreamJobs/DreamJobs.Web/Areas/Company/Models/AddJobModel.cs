58e93d82fb4f78f170ddc1efe887a80f65b90d67
Md Noyon Hossen <noyonhossen1996@gmail.com>
2021-03-09 21:18:12 +0600

    job post skill added

--- DreamJobs/DreamJobs.Web/Areas/Company/Models/AddJobModel.cs
+++ DreamJobs/DreamJobs.Web/Areas/Company/Models/AddJobModel.cs
@@ -32,36 +32,46 @@ namespace DreamJobs.Web.Areas.Company.Models
         public bool IsMaleApplicable { get; set; }
         public bool IsFemaleApplicable { get; set; }
         public bool IsOtherApplicable { get; set; }
-        [Required(ErrorMessage = "Please Enter Required Skills")]
-        public string SkillsRequired { get; set; }
         [Required(ErrorMessage = "Please Select Category")]
         public string Category { get; set; }
         [Required(ErrorMessage = "Please Enter Email")]
         public string EmailForApply { get; set; }
         public IList<Category> Categories { get; set; }
+        [Required(ErrorMessage = "Please Select at least 1 Skill")]
+        public List<string> SkillsRequired { get; set; }
+        public IList<Skill> SkillList { get; set; }
 
         private ICategoryService _categoryService;
         private IJobService _jobService;
         private ICompanyService _companyService;
+        private ISkillService _skillService;
 
         public AddJobModel()
         {
             _jobService = Startup.AutofacContainer.Resolve<IJobService>();
             _companyService = Startup.AutofacContainer.Resolve<ICompanyService>();
             _categoryService = Startup.AutofacContainer.Resolve<ICategoryService>();
+            _skillService = Startup.AutofacContainer.Resolve<ISkillService>();
             var today = DateTime.Today;
             DeadLine = today.AddDays(1);
         }
 
         public AddJobModel(IJobService jobService,
             ICompanyService companyService,
-            ICategoryService categoryService)
+            ICategoryService categoryService,
+            ISkillService skillService)
         {
             _jobService = jobService;
             _companyService = companyService;
             _categoryService = categoryService;
+            _skillService = skillService;
             var today = DateTime.Today;
             DeadLine = today.AddDays(1);
+        }
+
+        internal async Task LoadAllSkills()
+        {
+            SkillList = await _skillService.GetAllSkillAsync();
         }
 
         internal async Task GetAllCategoryAsync()
@@ -74,6 +84,7 @@ namespace DreamJobs.Web.Areas.Company.Models
             var user = await base.GetUserAsync(userName);
             var companyDetails = await _companyService.GetCompanyDetailsAsync(user.Id);
             var dateTime = DateTime.Now;
+            var jobSkills = new List<JobSkill>();
 
             var job = new Job
             {
@@ -96,10 +107,21 @@ namespace DreamJobs.Web.Areas.Company.Models
                 IsMaleApplicable = this.IsMaleApplicable,
                 IsFemaleApplicable = this.IsFemaleApplicable,
                 IsOtherApplicable = this.IsOtherApplicable,
-                SkillsRequired = this.SkillsRequired,
                 Category = this.Category,
                 EmailForApply = this.EmailForApply
             };
+
+            foreach (var skill in SkillsRequired)
+            {
+                var jobSkill = new JobSkill
+                {
+                    SkillId = Guid.Parse(skill),
+                    JobId = user.Id
+                };
+                jobSkills.Add(jobSkill);
+            }
+
+            job.JobSkills = jobSkills;
 
             await _jobService.AddAsync(job);
         }
