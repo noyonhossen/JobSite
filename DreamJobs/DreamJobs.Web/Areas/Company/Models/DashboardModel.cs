4d261b27b45d14027c9862c95864a27b5798bad2
Md Noyon Hossen <noyonhossen1996@gmail.com>
2021-03-12 19:37:34 +0600

    company dashboard completed

---
+++ DreamJobs/DreamJobs.Web/Areas/Company/Models/DashboardModel.cs
@@ -0,0 +1,38 @@
+﻿using Autofac;
+using DreamJobs.Framework.Services;
+using DreamJobs.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DreamJobs.Web.Areas.Company.Models
+{
+    public class DashboardModel : BaseModel
+    {
+        public int TotalJobPosted { get; set; }
+        private ICompanyService _companyService;
+        private IJobService _jobService;
+
+        public DashboardModel()
+        {
+            _companyService = Startup.AutofacContainer.Resolve<ICompanyService>();
+            _jobService = Startup.AutofacContainer.Resolve<IJobService>();
+        }
+
+        public DashboardModel(ICompanyService companyService,
+            IJobService jobService)
+        {
+            _companyService = companyService;
+            _jobService = jobService;
+        }
+
+        internal async Task GetTotalJobPostedByCompany(string userName)
+        {
+            var user = await base.GetUserAsync(userName);
+            var companyDetails = await _companyService.GetCompanyDetailsAsync(user.Id);
+
+            TotalJobPosted = await _jobService.GetCompanyJobCount(companyDetails);
+        }
+    }
+}
